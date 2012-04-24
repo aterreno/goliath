56bdce5f43f02829389bf5f9654cdef79eceea9d
Antonio Terreno <antonio.terreno@gmail.com>
2012-04-24 12:27:55 +0100

    printing time of the request with millis

--- goliath.rb
+++ goliath.rb
@@ -2,6 +2,7 @@ require 'goliath'
 
 class Hello < Goliath::API
   def response(env)
-    [200, {}, "Hello World #{env['HTTP_USER_AGENT']} from #{env['SERVER_SOFTWARE']} running at port:#{env['SERVER_PORT']}"]
+
+    [200, {}, "Hello World #{env['HTTP_USER_AGENT']} from #{env['SERVER_SOFTWARE']} running at port:#{env['SERVER_PORT']} @ #{Time.now.strftime("%H:%M:%S,%L")}"]
   end
 end
